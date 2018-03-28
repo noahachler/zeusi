315b620c6171cd7675fc857b7a8ada3f79d6d1e5
Noah Achler <noah.achler@emate.it>
2018-03-28 19:43:20 +0200

    first db test

---
+++ dbtest.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "10.130.36.169";
+$username = "zeusi";
+$password = "zeusi";
+$dbname = "zeusi";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, title FROM test";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["id"] . " - " . $row["title"] . "<br>";
+    }
+} else {
+    echo "Nessun risultato";
+}
+$conn->close();
+?>
--- phpinfo.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-phpinfo(); 
-/*
-echo 'Extensions: ';
-print_r(get_loaded_extensions());
-echo 'Current PHP version: ' . phpversion();
-*/
-?>
