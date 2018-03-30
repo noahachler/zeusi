3d257832242d935c7ed5349ef826cb351e9628f0
Noah Achler <n.achler@emate.it>
2018-03-30 15:21:38 +0200

    Drupal 8.2.1

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
+<?php
 
-<?php 
-phpinfo(); 
-/*
-echo 'Extensions: ';
-print_r(get_loaded_extensions());
-echo 'Current PHP version: ' . phpversion();
-*/
-?>
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
